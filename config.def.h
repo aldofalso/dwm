945889505cb140f760a8ebe4eb21a5fd2a183d0b
aldofalso <aldofalso02@gmail.com>
2020-10-13 15:51:39 +0200

    switch to spleen font

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminus:size=15" };
-static const char dmenufont[]       = "Terminus:size=15";
+static const char *fonts[]          = { "spleen:size=15" };
+static const char dmenufont[]       = "spleen:size=15";
 static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#282828";
 static const char col_gray3[]       = "#ebdbb2";
